f0d248930dc6f1eb8e2b5af34d3bdd4a26b9d296
Julian Sauer <julian_sauer@gmx.net>
2016-04-01 16:29:15 +0200

    Added a status variable to each round to see if it is running, halted or finished.

--- src/main/java/de/gmx/endermansend/interfaces/RoundInterface.java
+++ src/main/java/de/gmx/endermansend/interfaces/RoundInterface.java
@@ -7,13 +7,44 @@ import java.util.Collection;
 
 public abstract class RoundInterface {
 
+    public enum Status {
+        GAME_IS_RUNNING,
+        GAME_IS_HALTED,
+        GAME_HAS_FINISHED
+    }
+
     protected int roundNumber;
+    protected Status status;
 
     public RoundInterface(int roundNumber) {
         this.roundNumber = roundNumber;
+        status = Status.GAME_IS_RUNNING;
+    }
+
+    public boolean resumeGame() {
+        return changeStatusTo(Status.GAME_IS_RUNNING);
+    }
+
+    public boolean pauseGame() {
+        return changeStatusTo(Status.GAME_IS_HALTED);
+    }
+
+    public boolean quitGame() {
+        return changeStatusTo(Status.GAME_HAS_FINISHED);
     }
 
     public abstract boolean addLotteryEntry(Player player, int luckyNumber);
+
+    /**
+     * Finishes the round and returns the owners of tickets with matching luckyNumbers
+     *
+     * @param luckyNumber Number which won the round
+     * @return List of ticket owners
+     */
+    public Collection<String> finishRound(int luckyNumber) {
+        status = Status.GAME_HAS_FINISHED;
+        return getOwnersOf(luckyNumber);
+    }
 
     public int getRoundNumber() {
         return roundNumber;
@@ -24,5 +55,13 @@ public abstract class RoundInterface {
     public abstract Collection<Integer> getTicketsOf(Player player);
 
     public abstract Collection<String> getOwnersOf(int luckyNumber);
+
+    protected boolean changeStatusTo(Status status) {
+        if (this.status == Status.GAME_HAS_FINISHED ||
+                this.status == status)
+            return false;
+        this.status = status;
+        return true;
+    }
 
 }
